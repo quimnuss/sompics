6b952bd224e67cb7b328f261dd4aa229f3990f17
Quim Nuss Székely <quimnuss@altaveu>
2024-07-02 21:03:50 +0200

    new: weighted platform as path2d

--- actors/weight_platform_path.gd
+++ actors/weight_platform_path.gd
@@ -1,6 +1,7 @@
-extends PathFollow2D
+extends Path2D
 
-@onready var label = $AnimatableBody2D/Label
+@onready var label = $FollowPath2D/AnimatableBody2D/Label
+@onready var follow_path = $FollowPath2D
 
 var pics_on_platform : int = 0
 @export var required_pics : int = 1
@@ -13,14 +14,14 @@ func update_label():
     label.set_text(str(missing_pics))
 
 func _process(delta):
-    var current_progress : float = self.get_progress_ratio()
+    var current_progress : float = follow_path.get_progress_ratio()
     if pics_on_platform >= required_pics:
         if current_progress < 1:
             current_progress = min(1, current_progress + 0.2*delta)
-            self.set_progress_ratio(current_progress)
+            follow_path.set_progress_ratio(current_progress)
     elif current_progress > 0:
         current_progress = max(0, current_progress - 0.2*delta)
-        self.set_progress_ratio(current_progress)
+        follow_path.set_progress_ratio(current_progress)
 
 func _on_area_2d_body_entered(body):
     if body is Pic:
---
+++ scenes/level_2.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+@onready var weight_platform = $Node2D/WeightPlatform
+
+
+func _ready():
+    weight_platform.required_pics = len(Persistence.pics)
