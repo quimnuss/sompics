5774f4c99d227836e5c3d06c602dc3e6cf646d83
Quim Nuss Székely <quimnuss@altaveu>
2024-07-05 22:12:35 +0200

    fix: platforms

--- actors/pic.gd
+++ actors/pic.gd
@@ -79,6 +79,9 @@ func _physics_process(delta):
     # Add the gravity.
     if not is_on_floor():
         velocity.y += gravity * delta
+    else:
+        velocity.y = gravity
+
     var just_jumped : bool = Input.is_action_just_pressed(self.jump)
     if just_jumped and is_on_floor():
         velocity.y = JUMP_VELOCITY
