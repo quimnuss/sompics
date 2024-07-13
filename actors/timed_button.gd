b8e4ef87faebef92d3a39263879d837986ad9843
pol <pol.monso@somenergia.coop>
2024-07-13 23:40:09 +0200

    new: timed button

---
+++ actors/timed_button.gd
@@ -0,0 +1,15 @@
+extends Node2D
+@onready var label = $Label
+
+@export var start_time : int = 5*60
+
+const time_warp : float = 10
+
+var remaining_time : float = start_time
+
+
+func _process(delta):
+    remaining_time -= time_warp*delta
+    var mins : int = remaining_time/60
+    var secs : int = fmod(remaining_time,60)
+    label.set_text("%02d:%02d" % [mins, secs])
