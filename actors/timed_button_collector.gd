7668398a4e2594221afa42925b2e2398a35185c6
pol <pol.monso@somenergia.coop>
2024-07-14 21:14:32 +0200

    fix: disable workflows, wip: react to all stopped

--- actors/timed_button_collector.gd
+++ actors/timed_button_collector.gd
@@ -4,6 +4,7 @@ var total_time : int = 0
 
 signal time_update(total_time)
 signal time_up
+signal all_stopped
 
 func _ready():
     for timed_button : TimedButton in get_tree().get_nodes_in_group('timed_buttons'):
@@ -11,6 +12,14 @@ func _ready():
 
 func somebody_timeout():
     time_up.emit()
+
+func somebody_stopped():
+    var all_stopped = true
+    for timed_button : TimedButton in get_tree().get_nodes_in_group('timed_buttons'):
+        if timed_button.is_running:
+            all_stopped = false
+    if all_stopped:
+        all_stopped.emit()
 
 func _process(delta):
     var previous_total_time : int = total_time
