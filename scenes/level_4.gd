4a6719c8942ca4860b9358b4676d2e8f2cdf69bd
pol <pol.monso@somenergia.coop>
2024-07-15 19:13:37 +0200

    fix: level 4 platform not spawning

--- scenes/level_4.gd
+++ scenes/level_4.gd
@@ -3,9 +3,9 @@ extends Node2D
 @onready var tile_map = $TileMap
 
 func build_bridge():
-    tile_map.set_cells_terrain_connect(0, [Vector2i(23,17)], 0, 0)
+    tile_map.set_cells_terrain_connect(0, [Vector2i(23,17)], 0, 2)
     await get_tree().create_timer(1).timeout
-    tile_map.set_cells_terrain_connect(0, [Vector2i(22,17)], 0, 0)
+    tile_map.set_cells_terrain_connect(0, [Vector2i(22,17)], 0, 2)
 
 func _on_button_button_pressed():
     build_bridge()
--- scenes/playground.gd
+++ scenes/playground.gd
@@ -7,7 +7,7 @@ func _ready():
 
 func build_bridge():
     for i in range(4):
-        tile_map.set_cells_terrain_connect(0, [Vector2i(23-i,15)], 0, 0)
+        tile_map.set_cells_terrain_connect(0, [Vector2i(23-i,15)], 0, 2)
         await get_tree().create_timer(1).timeout
 
 func _input(event):
