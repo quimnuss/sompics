7d0de82822e684100136b38e7261aa700908cd23
Quim Nuss Székely <quimnuss@altaveu>
2024-08-17 01:16:21 +0200

    wip: animatable platform based on followpath

--- scenes/level_3.gd
+++ scenes/level_3.gd
@@ -3,5 +3,5 @@ extends Node2D
 @onready var weight_platform = $Node2D/WeightPlatform
 
 func _ready():
-    weight_platform.required_pics = len(Persistence.pics)
+    weight_platform.required_pics = max(2, len(Persistence.pics) - 5)
     weight_platform.update_label()
