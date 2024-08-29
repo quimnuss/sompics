4875845074cd890ec6df22768cf6dedf47187e16
Diego Quintana <daquintanav@gmail.com>
2024-08-29 11:12:05 -0400

    wip: add evil bee sprites and scene
    just a demo

--- actors/box_tower.gd
+++ actors/box_tower.gd
@@ -2,6 +2,6 @@ extends RigidBody2D
 
 
 func _integrate_forces(state):
-    var count = state.get_contact_count()
-    for i in count:
-        print("Contact #", i, ", impulse: ", state.get_contact_impulse(i))
+	var count = state.get_contact_count()
+	for i in count:
+		print("Contact #", i, ", impulse: ", state.get_contact_impulse(i))
