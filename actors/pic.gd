07d3b5d057d9bc51f8c341b674c28e2bbdc705d6
Diego Quintana <daquintanav@gmail.com>
2024-08-30 17:23:50 -0400

    new: add simple dialogs, scenes and actors
    this is a terrible commit and I'm sorry

--- actors/pic.gd
+++ actors/pic.gd
@@ -50,310 +50,310 @@ signal pic_exit
 signal pic_back
 
 func _ready():
-	head.set_person(person)
-	self.name = person
-	coyote_timer.wait_time = coyote_frames / 60.0
-	add_to_group('pics')
+    head.set_person(person)
+    self.name = person
+    coyote_timer.wait_time = coyote_frames / 60.0
+    add_to_group('pics')
 
-	self.move_left = self.move_left + '-' + person
-	self.move_right = self.move_right  + '-' + person
-	self.jump = self.jump  + '-' + person
-	self.down = self.down  + '-' + person
+    self.move_left = self.move_left + '-' + person
+    self.move_right = self.move_right  + '-' + person
+    self.jump = self.jump  + '-' + person
+    self.down = self.down  + '-' + person
 
-	for action : String in actions:
-		if not InputMap.has_action(action + '-' + person):
-			InputMap.add_action(action + '-' + person)
+    for action : String in actions:
+        if not InputMap.has_action(action + '-' + person):
+            InputMap.add_action(action + '-' + person)
 
-	if not attached_pics.is_empty():
-		ropes_attach()
+    if not attached_pics.is_empty():
+        ropes_attach()
 
 
 func enter_door():
-	self.set_physics_process(false)
-	get_node("CollisionShape2D").disabled = true
-	self.is_out = true
-	head.set_modulate(Color(1,1,1,0.25))
-	pic_exit.emit()
+    self.set_physics_process(false)
+    get_node("CollisionShape2D").disabled = true
+    self.is_out = true
+    head.set_modulate(Color(1,1,1,0.25))
+    pic_exit.emit()
 
 
 func exit_door():
-	self.set_physics_process(true)
-	get_node("CollisionShape2D").disabled = false
-	head.set_modulate(Color(1,1,1,1))
-	self.is_out = false
-	pic_back.emit()
+    self.set_physics_process(true)
+    get_node("CollisionShape2D").disabled = false
+    head.set_modulate(Color(1,1,1,1))
+    self.is_out = false
+    pic_back.emit()
 
 
 func get_is_controlled() -> bool:
-	return Persistence.pics[Persistence.active_pic] == person and is_possessed
+    return Persistence.pics[Persistence.active_pic] == person and is_possessed
 
 
 func _process(_delta):
-	var is_controlled : bool = get_is_controlled()
-	var just_jumped : bool = Input.is_action_just_pressed(jump) or Input.is_action_just_pressed('jump-q') and is_controlled
+    var is_controlled : bool = get_is_controlled()
+    var just_jumped : bool = Input.is_action_just_pressed(jump) or Input.is_action_just_pressed('jump-q') and is_controlled
 
-	if is_on_door and not self.is_out and just_jumped:
-		enter_door()
-	elif is_out and just_jumped:
-		exit_door()
+    if is_on_door and not self.is_out and just_jumped:
+        enter_door()
+    elif is_out and just_jumped:
+        exit_door()
 
 
 func hold(key_ : Key):
-	key = key_
+    key = key_
 
 
 func drop():
-	if key and is_instance_valid(key):
-		key.drop()
+    if key and is_instance_valid(key):
+        key.drop()
 
 
 func attach(pic : Pic):
-	attached_pics.append(pic)
-	rope_attach(pic)
+    attached_pics.append(pic)
+    rope_attach(pic)
 
 
 func rope_attach(attached_pic : Pic):
-	var rope : Line2D = Line2D.new()
-	rope.width = 2
-	rope.default_color = Color.BROWN
-	#rope.show_behind_parent = true
-	rope.z_as_relative = true
-	rope.z_index = -1
-	rope.add_point(self.global_position)
-	rope.add_point(attached_pic.global_position)
-	add_child(rope)
-	ropes.append(rope)
+    var rope : Line2D = Line2D.new()
+    rope.width = 2
+    rope.default_color = Color.BROWN
+    #rope.show_behind_parent = true
+    rope.z_as_relative = true
+    rope.z_index = -1
+    rope.add_point(self.global_position)
+    rope.add_point(attached_pic.global_position)
+    add_child(rope)
+    ropes.append(rope)
 
 
 func ropes_attach():
-	for attached_pic : Pic in attached_pics:
-		rope_attach(attached_pic)
+    for attached_pic : Pic in attached_pics:
+        rope_attach(attached_pic)
 
 
 func kill():
-	head.set_modulate(Color(0.3,0.3,0.3,1))
-	self.set_physics_process(false)
-	self.set_process(false)
-	animation_player.play('death')
+    head.set_modulate(Color(0.3,0.3,0.3,1))
+    self.set_physics_process(false)
+    self.set_process(false)
+    animation_player.play('death')
 
 
 func external_input(player : String, action : String, is_pressed : bool = true):
 
-	if player != self.person:
-		return
-	var input_action : String = action + '-' + player
-	prints("external input ", input_action)
-	if action == 'move_up':
-		input_action = 'jump-' + player
-	if is_pressed:
-		Input.action_press(input_action)
-	else:
-		Input.action_release(input_action)
+    if player != self.person:
+        return
+    var input_action : String = action + '-' + player
+    prints("external input ", input_action)
+    if action == 'move_up':
+        input_action = 'jump-' + player
+    if is_pressed:
+        Input.action_press(input_action)
+    else:
+        Input.action_release(input_action)
 
 
 func resolve_pushing(direction : float):
 
-	# evaluate -- not reliable
-	#is_pushing_left = false
-	#is_pushing_right = false
-	#if is_on_wall():
-		#if direction < 0 and get_wall_normal() == Vector2(1,0):
-			#is_pushing_left = true
-		#elif direction > 0 and get_wall_normal() == Vector2(-1,0):
-			#is_pushing_right = true
+    # evaluate -- not reliable
+    #is_pushing_left = false
+    #is_pushing_right = false
+    #if is_on_wall():
+        #if direction < 0 and get_wall_normal() == Vector2(1,0):
+            #is_pushing_left = true
+        #elif direction > 0 and get_wall_normal() == Vector2(-1,0):
+            #is_pushing_right = true
 
-	var dx = global_position.x - previous_position
-	previous_position = global_position.x
+    var dx = global_position.x - previous_position
+    previous_position = global_position.x
 
-	is_pushing_left = direction < 0 and dx == 0
-	is_pushing_right = direction > 0 and dx == 0
+    is_pushing_left = direction < 0 and dx == 0
+    is_pushing_right = direction > 0 and dx == 0
 
-	# consequences
-	var left_push : CollisionShape2D = $PushingArea2D/CollisionShape2D as CollisionShape2D
-	left_push.disabled = true
-	var push_sprite : Sprite2D = $PushingArea2D/Sprite2D as Sprite2D
-	push_sprite.visible = false
-	if is_pushing_left or is_pushing_right:
-		left_push.disabled = false
-		push_sprite.visible = true
+    # consequences
+    var left_push : CollisionShape2D = $PushingArea2D/CollisionShape2D as CollisionShape2D
+    left_push.disabled = true
+    var push_sprite : Sprite2D = $PushingArea2D/Sprite2D as Sprite2D
+    push_sprite.visible = false
+    if is_pushing_left or is_pushing_right:
+        left_push.disabled = false
+        push_sprite.visible = true
 
 
 func draw_ropes():
-	for i in range(len(attached_pics)):
-		var attached_pic : Pic = attached_pics[i] if i < len(attached_pics) else null
-		var rope : Line2D = ropes[i] if i < len(ropes) else null
-		if attached_pic and is_instance_valid(attached_pic):
-			rope.set_point_position(0, Vector2(0,0))
-			rope.set_point_position(1, attached_pic.global_position - self.global_position)
-			rope.visible = true
-		else:
-			rope.visible = false
+    for i in range(len(attached_pics)):
+        var attached_pic : Pic = attached_pics[i] if i < len(attached_pics) else null
+        var rope : Line2D = ropes[i] if i < len(ropes) else null
+        if attached_pic and is_instance_valid(attached_pic):
+            rope.set_point_position(0, Vector2(0,0))
+            rope.set_point_position(1, attached_pic.global_position - self.global_position)
+            rope.visible = true
+        else:
+            rope.visible = false
 
 func _physics_process(delta):
 
-	var is_controlled : bool = get_is_controlled()
+    var is_controlled : bool = get_is_controlled()
 
-	# doesn't work dunno why. Anyway get_is_controlled is enough for external controllers so... whatever
-	#if not is_possessed:
-		#if not is_on_floor():
-			#velocity.y += gravity * delta
-			#move_and_slide()
-		#return
+    # doesn't work dunno why. Anyway get_is_controlled is enough for external controllers so... whatever
+    #if not is_possessed:
+        #if not is_on_floor():
+            #velocity.y += gravity * delta
+            #move_and_slide()
+        #return
 
-	var just_jumped : bool = Input.is_action_just_pressed(jump) or Input.is_action_just_pressed('jump-q') and is_controlled
-	var flying_direction : Vector2 = Input.get_vector(move_left, move_right, jump, down)
-	var direction : float = Input.get_axis(move_left, move_right)
+    var just_jumped : bool = Input.is_action_just_pressed(jump) or Input.is_action_just_pressed('jump-q') and is_controlled
+    var flying_direction : Vector2 = Input.get_vector(move_left, move_right, jump, down)
+    var direction : float = Input.get_axis(move_left, move_right)
 
-	if is_controlled:
-		if direction == 0:
-			direction = Input.get_axis('move_left-q', 'move_right-q')
-		if flying_direction == Vector2.ZERO:
-			flying_direction = Input.get_vector('move_left-q', 'move_right-q', 'jump-q', 'down-q')
+    if is_controlled:
+        if direction == 0:
+            direction = Input.get_axis('move_left-q', 'move_right-q')
+        if flying_direction == Vector2.ZERO:
+            flying_direction = Input.get_vector('move_left-q', 'move_right-q', 'jump-q', 'down-q')
 
-	if is_flying:
-		velocity = flying_direction * SPEED
-		move_and_slide()
-		return
+    if is_flying:
+        velocity = flying_direction * SPEED
+        move_and_slide()
+        return
 
-	# attached
-	if not attached_pics.is_empty():
-		_physics_process_attached(delta, just_jumped, direction)
-		return
+    # attached
+    if not attached_pics.is_empty():
+        _physics_process_attached(delta, just_jumped, direction)
+        return
 
-	var grav_factor = 1
-	if velocity.y > 0:
-		grav_factor = 3
+    var grav_factor = 1
+    if velocity.y > 0:
+        grav_factor = 3
 
-	if not is_on_floor():
-		velocity.y += grav_factor * gravity * delta
+    if not is_on_floor():
+        velocity.y += grav_factor * gravity * delta
 
-	if just_jumped and (is_on_floor() or coyote):
-		velocity.y = JUMP_VELOCITY
-		is_jumping = true
-		jump_sound.play()
+    if just_jumped and (is_on_floor() or coyote):
+        velocity.y = JUMP_VELOCITY
+        is_jumping = true
+        jump_sound.play()
 
-	self.set_collision_layer_value(6, is_on_floor() or not is_jumping)
+    self.set_collision_layer_value(6, is_on_floor() or not is_jumping)
 
-	if direction:
-		velocity.x = direction * SPEED
+    if direction:
+        velocity.x = direction * SPEED
 
-	if attached_pics.is_empty():
-		if not direction:
-			velocity.x = 0 # move_toward(velocity.x, 0, SPEED)
+    if attached_pics.is_empty():
+        if not direction:
+            velocity.x = 0 # move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+    move_and_slide()
 
-	debug_velocity = self.velocity
+    debug_velocity = self.velocity
 
-	$Label.set_text("%.2f %.2f" % [velocity.x, velocity.y])
+    $Label.set_text("%.2f %.2f" % [velocity.x, velocity.y])
 
-	resolve_pushing(direction)
+    resolve_pushing(direction)
 
-	# box interaction
-	for i in get_slide_collision_count():
-		var c = get_slide_collision(i)
-		if c.get_collider() is RigidBody2D:
-			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
+    # box interaction
+    for i in get_slide_collision_count():
+        var c = get_slide_collision(i)
+        if c.get_collider() is RigidBody2D:
+            c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
 
-	if not is_on_floor() and was_on_floor and not is_jumping:
-		coyote = true
-		coyote_timer.start()
+    if not is_on_floor() and was_on_floor and not is_jumping:
+        coyote = true
+        coyote_timer.start()
 
-	if is_on_floor() and is_jumping:
-		is_jumping = false
+    if is_on_floor() and is_jumping:
+        is_jumping = false
 
-	if velocity.x > 0:
-		#$AnimatedSprite2d.flip_h = false
-		head.flip_h = false
-	elif velocity.x < 0:
-		#$AnimatedSprite2d.flip_h = true
-		head.flip_h = true
+    if velocity.x > 0:
+        #$AnimatedSprite2d.flip_h = false
+        head.flip_h = false
+    elif velocity.x < 0:
+        #$AnimatedSprite2d.flip_h = true
+        head.flip_h = true
 
-	was_on_floor = is_on_floor()
+    was_on_floor = is_on_floor()
 
 
 func _physics_process_attached(delta : float, just_jumped : bool, direction : float):
 
-	var grav_factor = 1
-	# disable when attached because it messes with spring equations
-	if velocity.y > 0 and attached_pics.is_empty():
-		grav_factor = 3
+    var grav_factor = 1
+    # disable when attached because it messes with spring equations
+    if velocity.y > 0 and attached_pics.is_empty():
+        grav_factor = 3
 
-	if not is_on_floor():
-		velocity.y += grav_factor * gravity * delta
+    if not is_on_floor():
+        velocity.y += grav_factor * gravity * delta
 
-	if just_jumped and (is_on_floor() or coyote):
-		velocity.y = JUMP_VELOCITY
-		is_jumping = true
-		jump_sound.play()
-	elif just_jumped and is_on_wall():
-		velocity.y = JUMP_VELOCITY
-		is_jumping = true
-		jump_sound.play()
+    if just_jumped and (is_on_floor() or coyote):
+        velocity.y = JUMP_VELOCITY
+        is_jumping = true
+        jump_sound.play()
+    elif just_jumped and is_on_wall():
+        velocity.y = JUMP_VELOCITY
+        is_jumping = true
+        jump_sound.play()
 
-	if direction and (is_on_floor() or is_on_wall() or is_jumping):
-		velocity.x = direction * SPEED
+    if direction and (is_on_floor() or is_on_wall() or is_jumping):
+        velocity.x = direction * SPEED
 
-	var rope_velocity : Vector2 = Vector2(0,0)
-	var K : float = 50
-	var cum_dl : Vector2 = Vector2.ZERO
-	for attached_pic : Pic in attached_pics:
-		if attached_pic and is_instance_valid(attached_pic) and self.global_position.distance_to(attached_pic.global_position) > ROPELENGTH-10:
-			var attached_vector : Vector2 = (attached_pic.global_position - self.global_position)
-			var attached_direction : Vector2 = attached_vector.normalized()
-			var dl = attached_vector.length() - ROPELENGTH
-			if dl > 0:
-				cum_dl += attached_vector - ROPELENGTH*attached_direction
-				var constrained_velocity = K*dl*attached_direction*delta
-				rope_velocity += constrained_velocity
+    var rope_velocity : Vector2 = Vector2(0,0)
+    var K : float = 50
+    var cum_dl : Vector2 = Vector2.ZERO
+    for attached_pic : Pic in attached_pics:
+        if attached_pic and is_instance_valid(attached_pic) and self.global_position.distance_to(attached_pic.global_position) > ROPELENGTH-10:
+            var attached_vector : Vector2 = (attached_pic.global_position - self.global_position)
+            var attached_direction : Vector2 = attached_vector.normalized()
+            var dl = attached_vector.length() - ROPELENGTH
+            if dl > 0:
+                cum_dl += attached_vector - ROPELENGTH*attached_direction
+                var constrained_velocity = K*dl*attached_direction*delta
+                rope_velocity += constrained_velocity
 
-	#rope_velocity = rope_velocity.clamp(Vector2.ONE*-50, Vector2.ONE*50)
-	velocity += rope_velocity
-	if cum_dl.length() > 0: # -- removed because is_jumping is on until floor hits
-		const C : float = 1
-		var damp : Vector2 = -C*velocity*delta
-		velocity += damp
-	elif not is_on_floor() and velocity.y > 0 and is_jumping:
-		velocity.y += 2 * gravity * delta
+    #rope_velocity = rope_velocity.clamp(Vector2.ONE*-50, Vector2.ONE*50)
+    velocity += rope_velocity
+    if cum_dl.length() > 0: # -- removed because is_jumping is on until floor hits
+        const C : float = 1
+        var damp : Vector2 = -C*velocity*delta
+        velocity += damp
+    elif not is_on_floor() and velocity.y > 0 and is_jumping:
+        velocity.y += 2 * gravity * delta
 
-	#velocity = velocity.clamp(Vector2.ONE*-600, Vector2.ONE*600)
+    #velocity = velocity.clamp(Vector2.ONE*-600, Vector2.ONE*600)
 
-	if not direction and abs(rope_velocity.x) < 0.01:
-		velocity.x = 0
-	elif not direction and is_on_floor(): # friction
-		velocity.x = move_toward(velocity.x, 0, 5*SPEED*delta)
+    if not direction and abs(rope_velocity.x) < 0.01:
+        velocity.x = 0
+    elif not direction and is_on_floor(): # friction
+        velocity.x = move_toward(velocity.x, 0, 5*SPEED*delta)
 
-	if len(ropes):
-		draw_ropes()
+    if len(ropes):
+        draw_ropes()
 
-	move_and_slide()
+    move_and_slide()
 
-	debug_velocity = self.velocity
+    debug_velocity = self.velocity
 
-	$Label.set_text("%.2f %.2f" % [velocity.x, velocity.y])
+    $Label.set_text("%.2f %.2f" % [velocity.x, velocity.y])
 
-	resolve_pushing(direction)
+    resolve_pushing(direction)
 
-	if not is_on_floor() and was_on_floor and not is_jumping:
-		coyote = true
-		coyote_timer.start()
+    if not is_on_floor() and was_on_floor and not is_jumping:
+        coyote = true
+        coyote_timer.start()
 
-	if (is_on_floor() or is_on_wall()) and is_jumping:
-		is_jumping = false
+    if (is_on_floor() or is_on_wall()) and is_jumping:
+        is_jumping = false
 
-	if velocity.x > 0:
-		#$AnimatedSprite2d.flip_h = false
-		head.flip_h = false
-	if velocity.x < 0:
-		#$AnimatedSprite2d.flip_h = true
-		head.flip_h = true
+    if velocity.x > 0:
+        #$AnimatedSprite2d.flip_h = false
+        head.flip_h = false
+    if velocity.x < 0:
+        #$AnimatedSprite2d.flip_h = true
+        head.flip_h = true
 
-	was_on_floor = is_on_floor()
+    was_on_floor = is_on_floor()
 
 
 func possess_toggle(new_is_possessed : bool):
-	is_possessed = new_is_possessed
+    is_possessed = new_is_possessed
 
 
 func _on_coyote_timer_timeout():
-	coyote = false
+    coyote = false
--- scenes/intermission_level.gd
+++ scenes/intermission_level.gd
@@ -1,14 +1,11 @@
-extends PathFollow2D
+extends Node2D
 
-var speed = 0.2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
+    Dialogic.start("res://dialogs/dr_back_timeline_ending.dtl")
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    loop_movement(delta)
-
-func loop_movement(delta):
-    progress_ratio += delta * speed
+    pass
