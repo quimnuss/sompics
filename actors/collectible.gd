76afc46052b82c38b71d7ca638224f24bfcec316
pol <pol.monso@somenergia.coop>
2024-09-06 17:13:04 +0200

    new: fita animation_player logic

--- actors/collectible.gd
+++ actors/collectible.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 @export var collectible_data : CollectibleData
 @onready var animation_player = $AnimationPlayer
+@onready var canvas_layer = $CanvasLayer
 
-# Called when the node enters the scene tree for the first time.
+var fita_consumed = false
+
 func _ready():
     if OS.is_debug_build() and get_tree().root == get_parent():
         var debug_camera : Camera2D = Camera2D.new()
@@ -11,7 +13,14 @@ func _ready():
         add_child(debug_camera)
     
     animation_player.play('idle')
+    canvas_layer.call_deferred('set_visible', false)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-    pass
+func _on_area_2d_body_entered(body):
+    if body is Pic and not fita_consumed:
+        fita_consumed = true
+        canvas_layer.visible = true
+        get_tree().call_group('pics', 'possess_toggle', false)
+
+func _on_button_pressed():
+    get_tree().call_group('pics', 'possess_toggle', true)
+    canvas_layer.visible = false
--- scripts/collectible_data.gd
+++ scripts/collectible_data.gd
@@ -2,7 +2,8 @@ extends Resource
 class_name CollectibleData
 
 @export var title : String
-@export var team : String
+@export_enum('Dades', 'ERP', 'Webapps', 'Suport', 'POs') var team : String
 @export var time : String
-@export var description : String
-@export var enabler : String
+@export_multiline var impacte : String
+@export_multiline var enabler : String
+@export_multiline var added_value : String
