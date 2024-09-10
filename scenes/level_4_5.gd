263227cb949d985a11cb14b97b211e16da639a52
Quim Nuss Székely <quimnuss@altaveu>
2024-09-10 21:12:34 +0200

    new: ce scene and death

---
+++ scenes/level_4_5.gd
@@ -0,0 +1,10 @@
+extends Node2D
+@onready var ce = $CE
+
+
+func _ready():
+    Dialogic.timeline_ended.connect(_on_dialogic_timeline_ended)
+
+
+func _on_dialogic_timeline_ended():
+    get_tree().create_tween().tween_property(ce, "modulate:a", 0.0, 2.0)
