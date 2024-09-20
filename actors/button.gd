8c5692ae3488eb834309881dcbf07316bbe7df00
Quim Nuss Székely <quimnuss@altaveu>
2024-09-20 10:09:18 +0200

    fix: disable button on pressed

--- actors/button.gd
+++ actors/button.gd
@@ -3,10 +3,13 @@ extends Node2D
 @onready var off = $Off
 @onready var on = $On
 
+var is_pressed : bool = false
+
 signal button_pressed
 
 func _on_area_2d_body_entered(body):
-    if body is Pic:
+    if not is_pressed and body is Pic:
+        is_pressed = true
         on.visible = true
         off.visible = false
         button_pressed.emit()
